95243b07e309286b4a53a7fb31a3f1c8e60f9769
Leeya Davis <leeya463@gmail.com>
2019-12-23 00:18:52 -0500

    95 percent done on twenty one

--- twenty_one.rb
+++ twenty_one.rb
@@ -2,29 +2,33 @@ require 'io/console'
 require 'pry'
 require 'pry-byebug'
 
-def prompt (message)
+def prompt(message)
   puts "=> #{message}"
+end
+
+def line
+  puts ""
 end
 
 def initialize_deck
   {
-  'H' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
-  'S' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
-  'D' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
-  'C' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-}
+    'H' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    'S' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    'D' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    'C' => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  }
 end
 
 def reset_cards(players_cards, dealers_cards)
-  players_cards = []
-  dealers_cards = []
+  players_cards.clear
+  dealers_cards.clear
 end
 
 def deal_card(cards, deck)
   suits = deck.keys
   selected_suit = suits.sample
   selected_card = deck[selected_suit].sample
-  
+
   cards << [selected_suit, selected_card]
   deck[selected_suit].delete(selected_card)
 
@@ -33,28 +37,41 @@ def deal_card(cards, deck)
   end
 end
 
-def initial_deal(cards, deck)
-  2.times do 
-    deal_card(cards, deck)
+def initial_deal(players_cards, dealers_cards, deck)
+  2.times do
+    deal_card(players_cards, deck)
   end
+
+  2.times do
+    deal_card(dealers_cards, deck)
+  end
+end
+
+def display_final_card_totals(players_cards, dealers_cards)
+  prompt "Your cards: #{total(players_cards)} | " \
+  "Dealer\'s cards: #{total(dealers_cards)}"
 end
 
 def display_cards(players_cards, dealers_cards)
-  prompt "Your cards total #{total(players_cards)}"
-  prompt "Dealer\'s card is worth #{display_dealers_cards(dealers_cards)}"
+  prompt "Your cards: #{total(players_cards)} | " \
+  "#{display_dealer_card(dealers_cards)}"
 end
 
-def display_dealers_cards(cards)
-  sum = 0
-  value = cards[0][1]
+def display_dealer_card(cards)
+  face_cards = { "A" => "Ace", "J" => "Jack", "Q" => "Queen", "K" => "King" }
+  suits = { "H" => "Hearts", "D" => "Diamonds",
+            "C" => "Clubs", "S" => "Spades" }
 
-  if value == "A"
-    sum += 11 
-  elsif value.to_i == 0 
-    sum += 10
-  else 
-    sum += value.to_i
-  end
+  suit = cards[0][0]
+  card = cards[0][1]
+
+  value = if face_cards.keys.include?(card) == true
+            face_cards[card]
+          else
+            card.to_s
+          end
+
+  "Dealer's card: #{value} of #{suits[suit]}"
 end
 
 def total(cards)
@@ -62,13 +79,13 @@ def total(cards)
 
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0
+             10
+           else
+             value.to_i
+           end
   end
 
   values.select { |value| value == "A" }.count.times do
@@ -90,79 +107,100 @@ def player_turn(players_cards, dealers_cards, deck)
     deal_card(players_cards, deck) if answer == 'h'
     break if answer == 's' || busted?(players_cards)
     display_cards(players_cards, dealers_cards)
-    # break if answer == 's' || busted?(players_cards)
   end
-  
+
   if busted?(players_cards)
-    false
+    prompt "Oh no! You went over!"
   else
-    prompt "You chose to stay. Dealer's turn!"
+    prompt "You choose to stay. Dealer's turn!"
+    line
   end
 end
 
-def dealer_turn(players_cards, dealers_cards, deck)
+def dealer_turn(dealers_cards, deck)
   loop do
     break if total(dealers_cards) >= 17 || busted?(dealers_cards)
     deal_card(dealers_cards, deck)
   end
 
   if busted?(dealers_cards)
-    false
+    sleep(1)
+    prompt "Dealer went over!"
   else
-    prompt 'Computer chose to stay!'
+    prompt 'Dealer chooses to stay!'
   end
+end
+
+def player_won?(player, dealer)
+  player <= 21 && player > dealer || dealer > 21 && dealer > player
+end
+
+def dealer_won?(player, dealer)
+  player > 21 && player > dealer || dealer <= 21 && dealer > player
 end
 
 def compare_cards(players_cards, dealers_cards)
   player = total(players_cards)
   dealer = total(dealers_cards)
-# not accounting for player busting but being higher than dealer
-  if player <= 21 && player > dealer 
-    'Player'
-  elsif player > 21 && player > dealer
-    'Dealer'
-  elsif dealer <= 21 && dealer > player
-    'Dealer'
-  elsif dealer > 21 && dealer > player
-    'Player'
+
+  if player_won?(player, dealer)
+    'You won!'
+  elsif dealer_won?(player, dealer)
+    'Dealer won!'
   end
 end
 
-def declare_winner(winner)
-  prompt "#{winner} won!"
+def declare_winner(players_cards, dealers_cards)
+  results = compare_cards(players_cards, dealers_cards)
+  if results
+    prompt results.to_s
+  else
+    prompt "It's a tie!"
+  end
+end
+
+def final_totals(players_cards, dealers_cards)
+  sleep(1)
+  prompt "The final totals are..."
+  display_final_card_totals(players_cards, dealers_cards)
+  line
+end
+
+def play_round(players_cards, dealers_cards, deck)
+  player_turn(players_cards, dealers_cards, deck)
+  return if busted?(players_cards)
+  dealer_turn(players_cards, dealers_cards, deck)
 end
 
 players_cards = []
 dealers_cards = []
-result = true
 
 system 'clear'
 prompt 'Welcome to Twenty One!' # flush out intro & explain gain
 
 loop do
-  loop do
-    reset_cards(players_cards, dealers_cards)
-    deck = initialize_deck
-    prompt 'Dealing cards...'
-    sleep(1)
+  system 'clear'
+  reset_cards(players_cards, dealers_cards)
+  deck = initialize_deck
 
-    puts ""
-    initial_deal(players_cards, deck)
-    initial_deal(dealers_cards, deck)
-    display_cards(players_cards, dealers_cards)
+  prompt 'Dealing cards...'
+  sleep(1)
+  line
 
-    puts ""
-    break unless player_turn(players_cards, dealers_cards, deck)
-    break unless dealer_turn(players_cards, dealers_cards, deck)
-    break
-  end
-  puts ""
-  prompt "The final score is..."
-  result = compare_cards(players_cards, dealers_cards)
+  initial_deal(players_cards, dealers_cards, deck)
   display_cards(players_cards, dealers_cards)
-  
-  declare_winner(result)
-  puts ""
+  line
+
+  play_round(players_cards, dealers_cards, deck)
+  line
+
+  final_totals(players_cards, dealers_cards)
+  sleep(2)
+  line
+
+  declare_winner(players_cards, dealers_cards)
+  line
+
   prompt 'Would you like to play again? (y/n)' # create #play_again?
   break if gets.chomp.downcase == 'n'
 end
