14e5351c65dbbf04ae07f5851f536b3b9a61be94
Leeya Davis <leeya463@gmail.com>
2019-12-24 22:22:02 -0500

    finish twenty one

--- twenty_one.md
+++ twenty_one.md
@@ -2,31 +2,10 @@ Make a note in notebook of:
 > The use of :symbols in #detect_results & #display_results and how the two methods "talked" to each other
 > Difference in break condition for #player_turn from 'break if answer.downcase == 's' to 'break if ['h', 's'].include?(answer)'
 > How 3 game exits were incorporated (look at the number of loops and breaks needed to completely exit game) 
+> Arbitrary code (WHATEVER_ONE, DEALER_HITS_UNTIL) should be made into constants, this way if we choose to change them we only have to go to one place!
+> calling a method repeatedly, like we did #total(cards) is expensive performance-wise. Better off using a local variable to cache the calculation rather than call it every single time
 
 Change code:
 > Remove 'requires' at the top of page
-> Don't add intro for game
-
-Some bonus features to consider:
-
-1. Calculating the total. Every time we need the player's or dealer's total value, we call a total method. Doing this is expensive, from a performance perspective (suppose that we cared about performance). Do we really need to calculate the total value over and over? Can we use a local variable to cache the calculation, and instead of calling total every time, just use the local variable? 
-
-We're unnecessarily calling total multiple times here. We can cache each player's total in a local variable within the main loop and just call those local variables instead, like this:
-
-player_total = total(player_cards)
-dealer_total = total(dealer_cards)
-Now we can replace each call to total with the appropriate local variable. This includes changing the busted? and detect_result methods to accept these new local variables instead of player_cards or dealer_cards.
-
-If we cache each player's total like this, will it continue to update correctly throughout the game? If not, at what point do we need to update each player's total?
 ------------------
-2. We use the play_again? three times: after the player busts, after the dealer busts, or after both participants stay and compare cards. Why is the last call to play_again? a little different from the previous two?
 
-3. Ending the round. As mentioned above, there are 3 places where the round can end and we call play_again? each time. But another improvement we'd like to make is to give it a consistent end-of-round output. Right now, we get a grand output only after comparing cards. Can we extract this to a method and use it in the other two end-of-round areas?
-
-4. Keep track of who won each round, and declare whoever reaches 5 points first as the winner.
-
-5. What if we wanted to change this game to Thirty-One, and the dealer hits until 27? Or what if our game should be Forty-One? Or Fifty-One? In other words, the two major values right now -- 21 and 17 -- are quite arbitrary. We can store them as constants and refer to the constants throughout the program. If we wanted to change the game to Whatever-One, it's just a matter of updating those constants.
-
-KEY DIFFERENCES IN SOLUTION CODE VS MINE
-3. Used the result of one method (#detect_result) as keys for another (#display_result)
-4. Display the scores when user/dealer has stayed
--- twenty_one.rb
+++ twenty_one.rb
@@ -1,6 +1,7 @@
 require 'io/console'
-require 'pry'
-require 'pry-byebug'
+
+WHATEVER_ONE = 21
+DEALER_HITS_UNTIL = 17
 
 def prompt(message)
   puts "=> #{message}"
@@ -68,91 +69,30 @@ def total(cards)
   sum
 end
 
-def busted?(cards)
-  total(cards) > 21
+def player_busted?
+  @player_total > WHATEVER_ONE
 end
 
-def player_turn(players_cards, dealers_cards, deck)
-  loop do
-    answer = nil
-    loop do
-      prompt "Do you want to (h)it or (s)tay?"
-      answer = gets.chomp
-      break if ['h', 's'].include?(answer)
-      prompt "Please enter a valid choice"
-    end 
-
-    if answer == 'h'  
-      deal_card(players_cards, deck)
-      prompt "You chose to hit!"
-      display_cards(players_cards, dealers_cards)
-    end
-  
-   break if answer == 's' || busted?(players_cards)
-  end 
-
-  if busted?(players_cards)
-    # display_results(players_cards, dealers_cards)
-    display_cards(players_cards, dealers_cards)
-    grand_output(players_cards, dealers_cards)
-    play_again?
-  else
-    prompt "You choose to stay. Dealer's turn!"
-    line
-  end
+def dealer_busted?
+  @dealer_total > WHATEVER_ONE
 end
 
-def dealer_turn(dealers_cards, deck)
-  loop do
-    break if total(dealers_cards) >= 17 || busted?(dealers_cards)
-    prompt "Dealer chooses to hit!"
-    deal_card(dealers_cards, deck)
-  end
-
-  if busted?(dealers_cards)
-    sleep(1)
-    prompt "Dealer went over!"
-    # display_results(players_cards, dealers_cards)
-    display_cards(players_cards, dealers_cards)
-    grand_output(players_cards, dealers_cards)
-    play_again?
-  else
-    prompt 'Dealer chooses to stay!'
-  end
-end
-
-def display_final_card_totals(players_cards, dealers_cards)
-  prompt "Your cards: #{players_cards} " \
-  "totaling #{total(players_cards)}"
-  prompt "Dealer's cards: #{dealers_cards} " \
-  "totaling #{total(dealers_cards)}"
-end
-
-def display_cards(players_cards, dealers_cards)
-  prompt "Your cards: #{players_cards} " \
-  "totaling #{total(players_cards)}"
-  prompt "Dealer's cards: #{dealers_cards[0]} and ?"
-end
-
-def detect_result(players_cards, dealers_cards) #compare_cards
-  player_total = total(players_cards)
-  dealer_total = total(dealers_cards)
-
-  if player_total > 21
+def detect_result
+  if @player_total > WHATEVER_ONE
     :player_busted
-  elsif dealer_total > 21
+  elsif @dealer_total > WHATEVER_ONE
     :dealer_busted
-  elsif dealer_total < player_total
+  elsif @dealer_total < @player_total
     :player
-  elsif player_total < dealer_total
+  elsif @player_total < @dealer_total
     :dealer
-  else 
+  else
     :tie
   end
 end
 
-def display_results(players_cards, dealers_cards)
-  result = detect_result(players_cards, dealers_cards)
+def display_results
+  result = detect_result
 
   case result
   when :player_busted
@@ -163,67 +103,196 @@ def display_results(players_cards, dealers_cards)
     prompt "You win!"
   when :dealer
     prompt "Dealer wins!"
-  else 
+  else
     prompt "It's a tie!"
   end
 end
 
-def final_totals(players_cards, dealers_cards)
-  sleep(1)
-  prompt "The final totals are..."
-  display_final_card_totals(players_cards, dealers_cards)
-  line
+def show_players_hand(players_cards)
+  prompt "Your cards: #{players_cards} total #{@player_total}"
+end
+
+def players_choice
+  answer = nil
+  loop do
+    prompt "Do you want to (h)it or (s)tay?"
+    answer = gets.chomp
+    break if ['h', 's'].include?(answer)
+    prompt "Please enter a valid choice"
+  end
+  answer
+end
+
+def hit(answer, players_cards, deck)
+  if answer == 'h'
+    deal_card(players_cards, deck)
+    @player_total = total(players_cards)
+
+    prompt "You choose to hit!"
+    show_players_hand(players_cards)
+    line
+  end
+end
+
+def player_turn(players_cards, dealers_cards, deck, scoreboard)
+  loop do
+    answer = players_choice
+    hit(answer, players_cards, deck)
+    break if answer == 's' || player_busted?
+  end
+
+  if player_busted?
+    @dealer_total = total(dealers_cards)
+    continue
+    grand_output(players_cards, dealers_cards, scoreboard)
+    play_again?
+  else
+    prompt "You choose to stay at #{@player_total}."
+  end
+end
+
+def show_dealers_hand(dealers_cards)
+  prompt "Dealer's cards: #{dealers_cards} total #{@dealer_total}"
+end
+
+def dealer_turn(dealers_cards, players_cards, deck, scoreboard)
+  @dealer_total = total(dealers_cards)
+
+  loop do
+    break if @dealer_total >= DEALER_HITS_UNTIL || dealer_busted?
+    prompt "Dealer chooses to hit!"
+
+    deal_card(dealers_cards, deck)
+    @dealer_total = total(dealers_cards)
+
+    show_dealers_hand(dealers_cards)
+    continue
+  end
+
+  if dealer_busted?
+    grand_output(players_cards, dealers_cards, scoreboard)
+    play_again?
+  else
+    prompt "Dealer chooses to stay at #{@dealer_total}"
+  end
+end
+
+def display_cards_and_totals(players_cards, dealers_cards)
+  show_players_hand(players_cards)
+  show_dealers_hand(dealers_cards)
+end
+
+def display_initial_cards(players_cards, dealers_cards)
+  @player_total = total(players_cards)
+
+  show_players_hand(players_cards)
+  prompt "Dealer's cards: #{dealers_cards[0]} and ?"
 end
 
 def play_again?
-  puts "-------------"
-  prompt "Do you want to play again? (y or n)"
-  answer = gets.chomp
+  answer = nil
+  loop do
+    puts "------------------------------------------------------"
+    prompt "Do you want to play again? (y or n)"
+    answer = gets.chomp.downcase
+    break if ['y', 'n'].include?(answer)
+    prompt "Please enter a valid choice"
+  end
   answer.downcase == 'y'
 end
 
-def grand_output(players_cards, players_cards)
-  final_totals(players_cards, dealers_cards)
+def grand_output(players_cards, dealers_cards, scoreboard)
+  puts "------------------------------------------------------"
+  prompt "End of round!"
+  display_cards_and_totals(players_cards, dealers_cards)
   sleep(2)
   line
 
-  display_results(players_cards, dealers_cards)
+  display_results
+  update_score(scoreboard)
+  display_score(scoreboard)
   line
+  grand_winner(scoreboard) if scoreboard.key(5)
+end
+
+def continue
+  line
+  prompt "Press any key to continue"
+  STDIN.getch
+  line
+end
+
+def reset_score(scoreboard)
+  scoreboard.transform_values! { |_| 0 }
+end
+
+def display_score(scoreboard)
+  symbols = scoreboard.keys
+  names = []
+
+  symbols.each do |el|
+    names << el.to_s.capitalize
+  end
+
+  prompt "Current score - #{names[0]}: #{scoreboard[symbols[0]]} |" \
+         " #{names[1]}: #{scoreboard[symbols[1]]}"
+end
+
+def update_score(scoreboard)
+  players = scoreboard.keys
+  result = detect_result
+
+  if result == :player || result == :dealer_busted
+    scoreboard[players[0]] += 1
+  elsif result == :dealer || result == :player_busted
+    scoreboard[players[1]] += 1
+  end
+end
+
+def grand_winner(scoreboard)
+  winner = scoreboard.key(5)
+  prompt "#{winner.to_s.capitalize} wins the game with 5 points!"
 end
 
 players_cards = []
 dealers_cards = []
+scoreboard = { player: 0, dealer: 0 }
 
 system 'clear'
 prompt 'Welcome to Twenty One!'
-sleep (1)
+continue
+
 loop do
-  system 'clear'
   reset_cards(players_cards, dealers_cards)
+  reset_score(scoreboard) if scoreboard.key(5)
   deck = initialize_deck
+  system 'clear'
 
   prompt 'Dealing cards...'
   sleep(1)
   line
 
   initial_deal(players_cards, dealers_cards, deck)
-  display_cards(players_cards, dealers_cards)
+  display_initial_cards(players_cards, dealers_cards)
   line
-  
-  player_return = player_turn(players_cards, dealers_cards, deck)
-  case player_return
-  when true 
+
+  case player_turn(players_cards, dealers_cards, deck, scoreboard)
+  when true
     next
   when false
     break
-  else
-    nil 
   end
-  sleep(1)
-  dealer_return = dealer_turn(dealers_cards, deck)
+
+  continue
+  case dealer_turn(dealers_cards, players_cards, deck, scoreboard)
+  when true
+    next
+  when false
+    break
+  end
   line
-  
-  grand_output(players_cards, players_cards)
+
+  grand_output(players_cards, dealers_cards, scoreboard)
 
   break unless play_again?
 end
